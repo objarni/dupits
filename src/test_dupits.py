9eaaeef41b5010a7076ad9f7ac8473b2f4d83c89
Olof Bjarnason <olof.bjarnason@gmail.com>
2015-06-10 14:25:33 +0200

    Implemented substring generation algorithm for single file

--- src/test_dupits.py
+++ src/test_dupits.py
@@ -1,10 +1,33 @@
-def test_finds_duplication_in_single_file(self):
-	dupFinder = DupFinder()
-	dupFinder.look_through('test.c', 'abc\ndef\abc\ndef\n')
-	report = dupFinder.generate_report()
-	self.assertEqual("""\
-This piece of code was found more than once:
-    abc
-    def
-""", report)
+# coding: utf-8
+import unittest
 
+# #Generera samtliga substrängar
+# Bygg chunkstruktur
+# Hitta intressant chunk (den med störst dup-värde!)
+# Skriv rapport
+
+
+def generate_substrings(s):
+    l = len(s)
+    for i in range(l):
+        for j in range(i, l):
+            yield s[i:j + 1]
+
+
+class TestGenerateSubstrings(unittest.TestCase):
+
+    def eq(self, g1, g2):
+        self.assertEqual(set(g1), set(g2))
+
+    def test_finds_all_substrings_of_short_string(self):
+        got = generate_substrings('abc')
+        expected = 'a,b,c,ab,bc,abc'.split(',')
+        self.eq(expected, got)
+
+    def test_finds_long_substring_in_big_string(self):
+        fatstring = ','.join(map(str, range(10 ** 2)))
+        result = generate_substrings(fatstring)
+        self.assertTrue(',45,' in result)
+
+if __name__ == '__main__':
+    unittest.main()
